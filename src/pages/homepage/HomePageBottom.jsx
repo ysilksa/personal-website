46376aa5c0e044e70336bc8b8376e8c98073ce35
ysilksa <isabella.yan.business@gmail.com>
2025-07-10 11:24:26 -0500

    finish home page text content

--- src/pages/homepage/HomePageBottom.jsx
+++ src/pages/homepage/HomePageBottom.jsx
@@ -7,10 +7,16 @@ const HomePageBottom = () => {
             <h1>Technical Skill Set</h1>
             <div className='skill-gallery'>
                 <div className='skill-container'>
+                    <h2>Languages</h2>
+                    <p>Java, JavaScript, C, C++, Python, HTML, CSS, SQL</p>
                 </div>
                 <div className='skill-container'>
+                    <h2>Frameworks</h2>
+                    <p>React, React + Vite, Node.js, Next.js</p>
                 </div>
                 <div className='skill-container'>
+                    <h2>Tools</h2>
+                    <p>Figma, Git, GitHub, Docusaurus</p>
                 </div>
             </div>
         </main>
