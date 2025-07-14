2d435a1d9e8db85cf4f3d33a8a36aac888f598a3
ysilksa <isabella.yan.business@gmail.com>
2025-07-14 14:38:21 -0500

    change to hash router

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/HomePage';
 
 function App() {
 
   return (
+    <Router>
       <Routes>
         <Route path="*" element={<HomePage />} />
         <Route path="/" element={<HomePage />} />
       </Routes>
+    </Router>
+      
   )
 }
 
