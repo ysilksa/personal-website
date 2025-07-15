75e8292edd78b7aec53010e9d3b4de5af220e337
ysilksa <isabella.yan.business@gmail.com>
2025-07-15 11:26:17 -0500

    change to only hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,17 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/HomePage';
 
 function App() {
 
   return (
-    <Router>
       <Routes>
         <Route path="*" element={<HomePage />} />
         <Route path="/" element={<HomePage />} />
       </Routes>
-    </Router>
-      
   )
 }
 
